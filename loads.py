9b92c7442d6817b89e324389781c37847b9f2ca3
Shams E Shifat <shamseshifat@gmail.com>
2021-05-21 04:45:12 +0600

    added `PointLoad` and `Moment` Classes in loads.py

--- loads.py
+++ loads.py
@@ -1,11 +1,13 @@
 class DIRECTION:
+    '''Global Sign Convention'''
     UP = 1
     DOWN = -1
     CW = 1
     ACW = -1
 
 
-class LOAD_TYPES:
+class SINGULARITY_EXPONENT:
+    '''<x - a>**n here n is singularity exponent'''
     UVL = 1
     UDL = 0
     POINT_LOAD = -1
@@ -15,16 +17,26 @@ class LOAD_TYPES:
 class LoadBase:
     ROTATION = -1
 
-    def __init__(self, type_, load_value=0, direction=DIRECTION.DOWN, start=0, end=0) -> None:
-        self.type_ = type_
-        self.load_value = load_value
-        self.pos0 = min(start, end) if end else max(start, end)
-        self.pos1 = max(start, end) if start and end else end
+    def __init__(self, exponent_, load_magnitude=0, direction=DIRECTION.DOWN, start=0, end=0, index_tuple=None) -> None:
+        '''index_tuple -> P|M|D|V , Class.IDX'''
+        self.exponent_ = exponent_
+        self.absolute_load_magnitude = load_magnitude
+        self.s = start
+        self.e = end
         self.direction = direction
+        self.idx_tuple = index_tuple
+
+    @property
+    def pos0(self):
+        return min(self.s, self.e) if self.e else max(self.s, self.e)
+
+    @property
+    def pos1(self):
+        return max(self.s, self.e) if self.s and self.e else self.s
 
     @property
     def load(self):
-        return self.load_value * self.direction
+        return self.absolute_load_magnitude * self.direction
 
     @property
     def span(self):
@@ -40,31 +52,63 @@ class LoadBase:
 
     @property
     def moment(self):
-        return self.point_load * self.moment_arm
+        return self.point_load * self.moment_arm if self.moment_arm else self.point_load
+
+    @property
+    def unique_id(self):
+        return self.idx_tuple[0] + str(self.idx_tuple[1])
+
+    def __repr__(self, child_name) -> str:
+        return f"ID:{self.unique_id}\n{child_name}(load={self.load}, pos0={self.pos0}, pos1={self.pos1},\n\tmoment={self.moment}, moment_arm={self.moment_arm},\n\tpoint_load={self.point_load},span={self.span},\n\texponent={self.exponent_})"
 
 
 class PointLoad(LoadBase):
-    TYPE = LOAD_TYPES.POINT_LOAD
+    EXPONENT = SINGULARITY_EXPONENT.POINT_LOAD
     END = 0
+    IDX = 0
+    SYMBOL = 'P'
 
-    def __init__(self, load_value, direction, position) -> None:
-        super().__init__(self.TYPE, load_value=load_value,
-                         direction=direction, start=position, end=self.END)
+    def __init__(self, load_magnitude, direction, position) -> None:
+        PointLoad.IDX += 1
+        super().__init__(self.EXPONENT, load_magnitude=load_magnitude,
+                         direction=direction, start=position, end=self.END, index_tuple=(self.SYMBOL, self.IDX))
+
+    def __repr__(self) -> str:
+        return super().__repr__(PointLoad.__name__)
 
 
 class Moment(LoadBase):
-    TYPE = LOAD_TYPES.MOMENT
+    EXPONENT = SINGULARITY_EXPONENT.MOMENT
     END = 0
+    IDX = 0
+    SYMBOL = 'M'
 
-    def __init__(self, load_value, direction, position) -> None:
-        super().__init__(self.TYPE, load_value=load_value,
-                         direction=direction, start=position, end=self.END)
+    def __init__(self, load_magnitude, direction, position) -> None:
+        Moment.IDX += 1
+        super().__init__(self.EXPONENT, load_magnitude=load_magnitude,
+                         direction=direction, start=position, end=self.END, index_tuple=(self.SYMBOL, self.IDX))
 
-        self.moment_arm = 0
+    @property
+    def moment_arm(self):
+        return 0
+
+    def __repr__(self) -> str:
+        return super().__repr__(Moment.__name__)
+
+
+class Udl(LoadBase):
+    EXPONENT = SINGULARITY_EXPONENT.UDL
+    IDX = 0
+    SYMBOL = 'D'
+    
+
 
 
 if __name__ == '__main__':
-    load = PointLoad(50, DIRECTION.UP, 15)
-    # print(dir(load))
-    print(load.__dict__)
-    print(load.moment)
+    p = PointLoad(50, DIRECTION.UP, 15)
+    p2 = PointLoad(100, DIRECTION.DOWN, 20)
+
+    m1 = Moment(220, DIRECTION.ACW, 40)
+    m2 = Moment(450, DIRECTION.CW, 6)
+
+    print(*[x.__dict__ for x in [p, p2, m1, m2]], sep='\n')
