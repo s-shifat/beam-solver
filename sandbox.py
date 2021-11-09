9f23633026c3b09ec697184488292ee5c5c744e4
Shams E Shifat <shamseshifat@gmail.com>
2021-11-10 01:27:48 +0600

    enabled sandbox.py to take json data as sys.argv

--- sandbox.py
+++ sandbox.py
@@ -2,11 +2,13 @@ from beam_package.loads.common_loads import *
 from beam_package.utils.sign_convention import DIRECTION
 from beam_package.beam.beam import Beam
 import json
+import sys
 
 
 # file = './beam_test_data.json'
 # file = './beam_test_data_2.json'
-file = './beam_test_data_4.json'
+#file = './beam_test_data_4.json'
+file = sys.argv[1]
 b = Beam(json_path=file)
 b.evaluate_json()
 print(b.load_table)
